fe777892d56a9d788277a9beedfbf82c156df25e
luciafava <luciafavak@gmail.com>
2021-05-19 15:34:29 +0200

    fix one and thow

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const app = {
     init: () => {
         conosole.log("app running");
+<<<<<<< HEAD
     }
     one: 1,
     two: 2
+=======
+    },
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4
+
+>>>>>>> parent of 4eb3038... fix master
 }
 
 setTimeout(app.init, 1000);
