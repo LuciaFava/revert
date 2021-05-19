4eb30389f2e9554457ab2baa3f653dbcd75d8896
luciafava <luciafavak@gmail.com>
2021-05-19 15:12:45 +0200

    fix master

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const app = {
     init: () => {
         conosole.log("app running");
     },
+<<<<<<< HEAD
     one: 1,
     two: 2,
     three: 3,
     four: 4
+=======
+>>>>>>> parent of 643e61e... one
 
 }
 
