9660869d68afafac4f390249d09a1c20492c2a31
luciafava <luciafavak@gmail.com>
2021-05-19 15:35:06 +0200

    new pulito

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 const app = {
     init: () => {
         conosole.log("app running");
-<<<<<<< HEAD
     }
-    one: 1,
-    two: 2
-=======
-    },
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4
-
->>>>>>> parent of 4eb3038... fix master
 }
 
 setTimeout(app.init, 1000);
