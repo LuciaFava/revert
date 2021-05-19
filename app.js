f7ef3881f503a104d6d4e9f033ea3a3ad904382a
luciafava <luciafavak@gmail.com>
2021-05-19 17:04:36 +0200

    fix js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const app = {
     init: () => {
         conosole.log("app running");
-<<<<<<< HEAD
     }
-=======
-    },
-    one: 1,
-    two: 2,
-    three: 3
-
->>>>>>> master2
 }
 
 setTimeout(app.init, 1000);
